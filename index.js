f5b69bb7cee1397cbb3ff14cd379a5ac41947d5e
tuguldur <aa402115@gmail.com>
2020-06-13 20:05:05 +0800

    fix on server

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ const app = express();
 require("dotenv").config();
 const PORT = process.env.PORT;
 const mongodb = process.env.MONGO;
+const path = require('path');
+if (process.env.DEV == 'false') app.use(express.static(path.join(__dirname, '/client/build')));
 app.use(express.json());
 app.use("/api", require("./src/routes"));
+if (process.env.DEV == 'false') app.get('*', (req, res) => res.sendFile(path.join(__dirname + '/client/build/index.html')));
 app.listen(PORT, () => {
   mongoose
     .connect(mongodb, {
